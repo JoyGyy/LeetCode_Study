155e571b7969459ddc898a34ae33a7aa2c610318
JoyGyy <k18372271132@outlook.com>
2025-10-15 18:09:58 +0800

    Update solution.java

--- Study/src/solution.java
+++ Study/src/solution.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class solution {
 
-    //P01
+    //P01 by joygy
     public int[] twoSum(int[] nums, int target) {
         int[] arr = new int[2];
         boolean sign = false;
@@ -15,13 +15,12 @@ public class solution {
                     break;
                 }
             }
-            if (sign)
-                break;
+            if (sign) break;
         }
         return arr;
     }
 
-    //P13
+    //P13 by AI
     public int romanToInt(String s) {
         Map<Character, Integer> romanMap = new HashMap<>();
         romanMap.put('I', 1);
@@ -47,8 +46,45 @@ public class solution {
         return result;
     }
 
-    //P14
+    //P14 by joygy
     public String longestCommonPrefix(String[] strs) {
+        String res = "";
+        String f_str = strs[0];
+        for (int i = 0; i < f_str.length(); i++) {
+            for (String str : strs) {
+                if (i >= str.length() || f_str.charAt(i) != str.charAt(i)) {
+                    return res;
+                }
+            }
+            res += f_str.charAt(i);
+        }
+        return res;
+    }
 
+    //P20 by AI
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    //P26 by joygy
+    public int removeDuplicates(int[] nums) {
+        List<int[]> list = List.of(nums);
     }
 }
